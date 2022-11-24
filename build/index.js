dd904ab10aaa8ad315fe3e365405eec3d8fccfd8
zeskord <zeskord@gmail.com>
2022-11-24 10:39:15 +0300

    Добавил настройки, пока не работает.

--- build/index.js
+++ build/index.js
@@ -4,8 +4,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT;
+// Читаем настройки SSL.
+const sslOptions = {
+    key: fs_1.default.readFileSync('/etc/letsencrypt/live/zeskord.site/privkey.pem'),
+    cert: fs_1.default.readFileSync('/etc/letsencrypt/live/zeskord.site/cert.pem'),
+    ca: fs_1.default.readFileSync('/etc/letsencrypt/live/zeskord.site/chain.pem')
+};
+app.use((req, res, next) => {
+    req.secure ? next() : res.redirect('https://' + req.headers.host + req.url);
+});
+app.get('*', (req, res) => {
+    res.sendFile(path_1.default.resolve(__dirname, 'planning-client', 'build', 'index.html'));
+});
 app.get("/", (req, res) => {
     res.send("OK");
+});
+app.listen(port, () => {
+    console.log(`Сервер запущен, порт ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,46 @@
-import express from "express"
+import express, { Express, Request, Response } from "express"
+import fs from "fs"
+import path from "path"
+import http from "http"
+import https from "https"
+import config from "config"
 
-const app = express()
-const port = 3000
+const app: Express = express()
+const port = config.get("port")
 
-app.get("/", (req, res) => {
+const useSsl: boolean = config.get("useSsl")
+
+if (useSsl === true) {
+
+    // Читаем настройки SSL.
+    const sslOptions = {
+        key: fs.readFileSync(config.get("sslOptions.key")),
+        cert: fs.readFileSync(config.get("sslOptions.cert")),
+        ca: fs.readFileSync(config.get("sslOptions.ca")),
+    }
+
+    // Перенаправляю на защищенный протокол, это нужно, чтобы работал телеграм бот.
+    app.use((req: Request, res: Response, next) => {
+        req.secure ? next() : res.redirect("https://" + req.headers.host + req.url)
+    })
+
+    https.createServer(sslOptions, app).listen(port)
+
+} else {
+
+    http.createServer(app).listen(port)
+
+}
+
+// Отдаем клиент.
+app.get("*", (req: Request, res: Response) => {
+    res.sendFile(
+        path.resolve(__dirname, "planning-client", "build", "index.html")
+    )
+})
+
+app.get("/", (req: Request, res: Response) => {
     res.send("OK")
 })
+
+
